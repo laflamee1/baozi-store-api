cf417037a4ee4293a7c4b6a3f3ebf85ec6c8dc17
laflamee1 <eliascer9@gmail.com>
2026-03-12 21:10:06 -0300

    Update BaoziStoreApiApplication.java
    Projeto final Baozi Store API

--- baozi-store-api/src/main/java/com/baozistore/baozi_store_api/BaoziStoreApiApplication.java
+++ baozi-store-api/src/main/java/com/baozistore/baozi_store_api/BaoziStoreApiApplication.java
@@ -1,7 +1,7 @@
 package com.baozistore.baozi_store_api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;	
 
 @SpringBootApplication
 public class BaoziStoreApiApplication {
